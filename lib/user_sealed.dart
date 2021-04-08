b4af9e477afb27047660a403108efd32ef112d08
goandreus <andres.chavez.crisostomo@gmail.com>
2021-04-07 23:42:50 -0500

    user sealed frezzed (#7)

--- lib/user_sealed.dart
+++ lib/user_sealed.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'user_sealed.freezed.dart';
 
-@frezzed
+@freezed
 abstract class User with _$User {
   const factory User.admin({String name}) = _Admin;
   const factory User.developer({String name}) = _Developer;
